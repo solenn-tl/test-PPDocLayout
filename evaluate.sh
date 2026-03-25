5c919f025c71d75ffeca628017300af806f26f39
solenn-tl <solenn.tual@gmail.com>
2026-03-25 20:10:56 +0100

    Documentation

--- evaluate.sh
+++ evaluate.sh
@@ -1,5 +1,7 @@
 #Evaluation 
 ## Mettre à jour le chemin vers le modèle à tester si besoin
+## Pour l'instant, ne fonctionne pas car le modèle  entraîné sur une epoch 
+## prédit plus de classes que celles attendues dans le dataset (ce qui créé une erreur)
 python main.py -c paddlex/configs/modules/layout_detection/PP-DocLayout-L.yaml \
     -o Global.mode=evaluate \
     -o Global.dataset_dir=./../../dataset_test \
